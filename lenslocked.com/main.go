8431bd4bbb9a76f59a180785e255367faab62a94
Panshul Gupta <panshul007@gmail.com>
2017-12-03 23:09:02 +0100

    add: inital commit with views

---
+++ lenslocked.com/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"gophercise/lenslocked.com/views"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var (
+	homeView *views.View
+	contactView *views.View
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	err := homeView.Template.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	err := contactView.Template.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+
+	homeView = views.NewView("views/home.gohtml")
+	contactView = views.NewView("views/contact.gohtml")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
+}
