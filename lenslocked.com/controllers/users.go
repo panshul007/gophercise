a886d86224b13c11c32663b714d0198af9aebd86
panshul.gupta <panshul.gupta@masterpayment.com>
2017-12-08 16:50:58 +0100

    update: user controller to parse form submit data.

--- lenslocked.com/controllers/users.go
+++ lenslocked.com/controllers/users.go
@@ -19,11 +19,18 @@ type Users struct {
 // This is used to render the form to signup new user accounts
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	u.NewView.Render(w, nil)
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 // This is used to process signup form for creating new user.
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, r.PostForm["email"])
+	fmt.Fprintln(w, r.PostForm["password"])
 	fmt.Fprintln(w, "Created a user account")
 }
