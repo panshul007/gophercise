c37c953c428a0ccf6fbd65bdbf4b41ea6e3db509
Panshul Gupta <panshul007@gmail.com>
2017-12-27 18:42:35 +0100

    add: and initialize user service

--- lenslocked.com/main.go
+++ lenslocked.com/main.go
@@ -3,14 +3,29 @@ package main
 import (
 	"net/http"
 
+	"fmt"
 	"github.com/gorilla/mux"
 	"gophercise/lenslocked.com/controllers"
+	"gophercise/lenslocked.com/models"
 )
 
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "postgres"
+	dbname = "lenslocked_dev"
+)
 
 func main() {
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable", host, port, user, dbname)
+	us, err := models.NewUserService(psqlInfo)
+	must(err)
+	defer us.Close()
+	us.AutoMigrate()
+
 	staticC := controllers.NewStatic()
-	usersC := controllers.NewUsers()
+	usersC := controllers.NewUsers(us)
 
 	r := mux.NewRouter()
 	r.Handle("/", staticC.Home).Methods("GET")
