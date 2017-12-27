29a1f623c65f20787e9e9529e9987f6214bf1096
Panshul Gupta <panshul007@gmail.com>
2017-12-27 18:44:04 +0100

    add: Name field and create user in the DB using the user service.

--- lenslocked.com/controllers/users.go
+++ lenslocked.com/controllers/users.go
@@ -1,23 +1,27 @@
 package controllers
 
 import (
-	"net/http"
-	"gophercise/lenslocked.com/views"
 	"fmt"
+	"gophercise/lenslocked.com/models"
+	"gophercise/lenslocked.com/views"
+	"net/http"
 )
 
-func NewUsers() *Users {
-	return &Users {
+func NewUsers(us *models.UserService) *Users {
+	return &Users{
 		NewView: views.NewView("bootstrap", "users/new"),
+		us:      us,
 	}
 }
 
 type Users struct {
 	NewView *views.View
+	us      *models.UserService
 }
 
 type SignupForm struct {
-	Email string `schema:"email"`
+	Name     string `schema:"name"`
+	Email    string `schema:"email"`
 	Password string `schema:"password"`
 }
 
@@ -35,6 +39,14 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	var form SignupForm
 	if err := parseForm(r, &form); err != nil {
 		panic(err)
+	}
+	user := models.User{
+		Name:  form.Name,
+		Email: form.Email,
+	}
+	if err := u.us.Create(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	fmt.Fprintln(w, form)
 }
