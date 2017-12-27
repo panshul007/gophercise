28e8e0466781b8a08e18dd0e2b507607b122eb00
Panshul Gupta <panshul007@gmail.com>
2017-12-27 14:32:28 +0100

    add: test for user model.

---
+++ lenslocked.com/models/users_test.go
@@ -0,0 +1,55 @@
+package models
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func testingUserService() (*UserService, error) {
+	const (
+		host   = "localhost"
+		port   = 5432
+		user   = "postgres"
+		dbname = "lenslocked_test"
+	)
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable", host, port, user, dbname)
+	us, err := NewUserService(psqlInfo)
+	if err != nil {
+		return nil, err
+	}
+	us.db.LogMode(false)
+	// Clear the users table between tests
+	us.DestructiveReset()
+	return us, nil
+}
+
+func TestUserService_Create(t *testing.T) {
+	us, err := testingUserService()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	us.DestructiveReset()
+	user := User{
+		Name:  "Michael Scott",
+		Email: "michael@email.io",
+	}
+
+	err = us.Create(&user)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if user.ID == 0 {
+		t.Errorf("Expected ID > 0. Received %d", user.ID)
+	}
+
+	if time.Since(user.CreatedAt) > time.Duration(5*time.Second) {
+		t.Errorf("Expected CreatedAt to be recent. Received %s", user.CreatedAt)
+	}
+
+	if time.Since(user.UpdatedAt) > time.Duration(5*time.Second) {
+		t.Errorf("Expected CreatedAt to be recent. Received %s", user.UpdatedAt)
+	}
+}
