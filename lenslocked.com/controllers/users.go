b6ef6a21ac7fce4bc721a449ee8cd8e5ea4f3fe1
Panshul Gupta <panshul007@gmail.com>
2017-12-28 00:16:08 +0100

    add: signin method to store cookie and redirect to cookietest url.

--- lenslocked.com/controllers/users.go
+++ lenslocked.com/controllers/users.go
@@ -51,7 +51,9 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprintln(w, user)
+
+	signIn(w, &user)
+	http.Redirect(w, r, "/cookietest", http.StatusFound)
 }
 
 type LoginForm struct {
@@ -80,12 +82,16 @@ func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	signIn(w, user)
+	http.Redirect(w, r, "/cookietest", http.StatusFound)
+}
+
+func signIn(w http.ResponseWriter, user *models.User) {
 	cookie := http.Cookie{
 		Name:  "email",
 		Value: user.Email,
 	}
 	http.SetCookie(w, &cookie)
-	fmt.Fprintln(w, user)
 }
 
 // CookieTest is used to display cookies set on the current user.
