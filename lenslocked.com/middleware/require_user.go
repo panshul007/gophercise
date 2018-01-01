5bf833c1ab8be4f72919a7d4c9aedd540d79e3fa
Panshul Gupta <panshul007@gmail.com>
2018-01-01 23:03:12 +0100

    update: set user to context if logged in for all requests.

--- lenslocked.com/middleware/require_user.go
+++ lenslocked.com/middleware/require_user.go
@@ -7,28 +7,50 @@ import (
 	"gophercise/lenslocked.com/models"
 )
 
-type RequireUser struct {
+type User struct {
 	models.UserService
 }
 
-func (mw *RequireUser) Apply(next http.Handler) http.HandlerFunc {
-	return mw.ApplyFn(next.ServeHTTP)
+func (u *User) Apply(next http.Handler) http.HandlerFunc {
+	return u.ApplyFn(next.ServeHTTP)
 }
 
-func (mw *RequireUser) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
+func (u *User) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cookie, err := r.Cookie("remember_token")
 		if err != nil {
-			http.Redirect(w, r, "/login", http.StatusFound)
+			next(w, r)
 			return
 		}
-		user, err := mw.UserService.ByRemember(cookie.Value)
+		user, err := u.UserService.ByRemember(cookie.Value)
 		if err != nil {
-			http.Redirect(w, r, "/login", http.StatusFound)
+			next(w, r)
+			return
 		}
 		ctx := r.Context()
 		ctx = context.WithUser(ctx, user)
 		r = r.WithContext(ctx)
+		next(w, r)
+	})
+}
+
+type RequireUser struct {
+	User
+}
+
+// Apply assumes that User middleware has already been run.
+func (mw *RequireUser) Apply(next http.Handler) http.HandlerFunc {
+	return mw.ApplyFn(next.ServeHTTP)
+}
+
+// ApplyFn assumes that User middleware has already been run.
+func (mw *RequireUser) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := context.User(r.Context())
+		if user == nil {
+			http.Redirect(w, r, "/login", http.StatusFound)
+			return
+		}
 		next(w, r)
 	})
 }
