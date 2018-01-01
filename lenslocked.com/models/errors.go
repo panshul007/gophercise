19687d510c67b681601b8e7e19cbd679b4c63195
Panshul Gupta <panshul007@gmail.com>
2018-01-01 16:40:27 +0100

    add: private error type.

--- lenslocked.com/models/errors.go
+++ lenslocked.com/models/errors.go
@@ -4,16 +4,19 @@ import "strings"
 
 const (
 	// ErrNotFound Error returned when resource not found.
-	ErrNotFound              modelError = "models: resource not found"
-	ErrIDInvalidId           modelError = "models: ID provided was invalid"
-	ErrPasswordIncorrect     modelError = "models: incorrect password provided"
-	ErrEmailRequired         modelError = "models: email address is required"
-	ErrEmailInvalid          modelError = "models: email address is not valid"
-	ErrEmailTaken            modelError = "models: email address is already taken"
-	ErrPasswordTooShort      modelError = "models: password must be at least 8 characters log"
-	ErrPasswordRequired      modelError = "models: password is required"
-	ErrRememberTokenTooShort modelError = "models: remember token should be at least 32 bytes"
-	ErrRememberTokenRequired modelError = "models: remember token is required"
+	ErrNotFound          modelError = "models: resource not found"
+	ErrPasswordIncorrect modelError = "models: incorrect password provided"
+	ErrEmailRequired     modelError = "models: email address is required"
+	ErrEmailInvalid      modelError = "models: email address is not valid"
+	ErrEmailTaken        modelError = "models: email address is already taken"
+	ErrPasswordTooShort  modelError = "models: password must be at least 8 characters log"
+	ErrPasswordRequired  modelError = "models: password is required"
+	ErrTitleRequired     modelError = "models: title is required"
+
+	ErrIDInvalidId           privateError = "models: ID provided was invalid"
+	ErrRememberTokenTooShort privateError = "models: remember token should be at least 32 bytes"
+	ErrRememberTokenRequired privateError = "models: remember token is required"
+	ErrUserIDRequired        privateError = "models: user ID is required"
 )
 
 type modelError string
@@ -27,4 +30,10 @@ func (e modelError) Public() string {
 	split := strings.Split(s, " ")
 	split[0] = strings.Title(split[0])
 	return strings.Join(split, " ")
+}
+
+type privateError string
+
+func (e privateError) Error() string {
+	return string(e)
 }
