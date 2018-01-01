9563ec97c72026fb2c046824a8020d4dd0d92e9b
Panshul Gupta <panshul007@gmail.com>
2018-01-01 18:30:31 +0100

    remove: print ln.

--- lenslocked.com/middleware/require_user.go
+++ lenslocked.com/middleware/require_user.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
 
 	"gophercise/lenslocked.com/context"
@@ -30,7 +29,6 @@ func (mw *RequireUser) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
 		ctx := r.Context()
 		ctx = context.WithUser(ctx, user)
 		r = r.WithContext(ctx)
-		fmt.Println("User found: ", user)
 		next(w, r)
 	})
 }
