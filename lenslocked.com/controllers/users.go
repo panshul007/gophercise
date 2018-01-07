291f5a06f37550bbcc9ee59289a16b4da24c8031
Panshul Gupta <panshul007@gmail.com>
2018-01-07 19:39:58 +0100

    add: redirect with alert.

--- lenslocked.com/controllers/users.go
+++ lenslocked.com/controllers/users.go
@@ -65,7 +65,13 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/login", http.StatusFound)
 		return
 	}
-	http.Redirect(w, r, "/galleries", http.StatusFound)
+
+	alert := views.Alert{
+		Level:   views.AlertLevelSuccess,
+		Message: "Welcome to LensLocked.com!",
+	}
+
+	views.RedirectAlert(w, r, "/galleries", http.StatusFound, alert)
 }
 
 type LoginForm struct {
--- lenslocked.com/views/view.go
+++ lenslocked.com/views/view.go
@@ -61,6 +61,12 @@ func (v *View) Render(w http.ResponseWriter, r *http.Request, data interface{}) 
 			Yield: data,
 		}
 	}
+
+	if alert := getAlert(r); alert != nil && vd.Alert == nil {
+		vd.Alert = alert
+		clearAlert(w)
+	}
+
 	vd.User = context.User(r.Context())
 	var buf bytes.Buffer
 
